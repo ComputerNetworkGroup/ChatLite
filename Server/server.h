98258d99e204968f847a1e0d73cc2cad5375a1bb
harrypotterrrr <jiahaolin19971119@126.com>
2018-12-19 23:02:01 +0800

    :muscle: framework of log in, parsing username & Database & code grammar is to be done!

--- Server/server.h
+++ Server/server.h
@@ -1,8 +1,21 @@
 #include <fstream>
 #include <sys/prctl.h>
 #include "myutil.h"
+#include <set>
+#define MAX_CONNECT 1000
 
-#define SERVER_MAX_CONNECT   1024
-#define MAX_LISTEN 1000
 
-void serverInit();
+struct ClientInfo{
+    bool alive;
+    int cfd;
+    string name;
+    ClientInfo(int _cfd = -1){
+        cfd = _cfd;
+        alive = false;
+    }
+};
+
+std::vector<ClientInfo> clientList; // 客户端列表
+std::vector<bool> firstLog;         // 客户端首次登陆标记数组，下标与map映射的k一致
+std::set<int> userOffline;          // 下线用户集合
+std::map<string, int> mapIndex;     // 用户名 --映射--> 下线用户集合index
