9e69084c90b68753fa40dfdb19f1495a22c92988
dzerzhinski01 <37956975+dzerzhinski01@users.noreply.github.com>
2018-12-22 22:10:37 +0800

    add settings

--- mysql/sql_setup.sql
+++ mysql/sql_setup.sql
@@ -1,6 +1,6 @@
-drop DATABASE if EXISTS test; 
-create DATABASE test;
-use test;
+drop DATABASE if EXISTS db1651025; 
+create DATABASE db1651025;
+use db1651025;
 
 CREATE TABLE userlist(
 id varchar(32) PRIMARY KEY,
@@ -10,8 +10,10 @@ log_time DATETIME
 
 CREATE table settings(
 id varchar(32) PRIMARY KEY,
-set_color INTEGER
-
+set_fontcolor INTEGER,
+set_color INTEGER,
+set_msgnum INTEGER,
+set_fontsize INTEGER
 );
 
 
@@ -27,9 +29,18 @@ CREATE TABLE test(
 id VARCHAR(32)
 );
 
-
-insert into userlist values("1",MD5('1'),now());
-insert into userlist values("2",MD5('2'),now());
-insert into userlist values("3",MD5('3'),now());
-insert into userlist values("4",MD5('4'),now());
-insert into userlist values("5",MD5('5'),now());
+insert into userlist values("1",MD5('1'),'1970-1-1 00:00:00');
+insert into settings values("1",0,0,0,0);
+insert into userlist values("2",MD5('2'),'1970-1-1 00:00:00');
+insert into settings values("2",0,0,0,0);
+insert into userlist values("3",MD5('3'),'1970-1-1 00:00:00');
+insert into settings values("3",0,0,0,0);
+insert into userlist values("4",MD5('4'),'1970-1-1 00:00:00');
+insert into settings values("4",0,0,0,0);
+insert into userlist values("5",MD5('5'),'1970-1-1 00:00:00');
+insert into settings values("5",0,0,0,0);
+insert into test values("1");
+insert into test values("2");
+insert into test values("3");
+insert into test values("4");
+insert into test values("5");
