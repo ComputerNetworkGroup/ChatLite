bcdc314c986c2a151e535651111b1ade2bf782ff
dzerzhinski01 <37956975+dzerzhinski01@users.noreply.github.com>
2018-12-23 13:35:38 +0800

    Update sql_setup.sql

--- mysql/sql_setup.sql
+++ mysql/sql_setup.sql
@@ -1,19 +1,20 @@
-drop DATABASE if EXISTS db1651025; 
-create DATABASE db1651025;
+DROP DATABASE IF EXISTS db1651025; 
+CREATE DATABASE db1651025;
 use db1651025;
 
 CREATE TABLE userlist(
-id varchar(32) PRIMARY KEY,
-passwd varchar(32),
+id VARCHAR(32) PRIMARY KEY,
+passwd VARCHAR(32),
 log_time DATETIME 
 );
 
-CREATE table settings(
-id varchar(32) PRIMARY KEY,
+CREATE TABLE settings(
+id VARCHAR(32) PRIMARY KEY,
 set_fontcolor INTEGER,
 set_color INTEGER,
 set_msgnum INTEGER,
-set_fontsize INTEGER
+set_fontsize INTEGER,
+FOREIGN KEY (id) REFERENCES userlist(id)
 );
 
 
@@ -21,13 +22,13 @@ CREATE TABLE msgs(
 from_id VARCHAR(32),
 to_id VARCHAR(32),
 msg_time DATETIME ,
-msg varchar(140),
-key msg_time(msg_time)
+msg VARCHAR(140),
+KEY order_msg_time(from_id,to_id,msg_time),
+FOREIGN KEY (from_id) REFERENCES userlist(id),
+FOREIGN KEY (to_id) REFERENCES userlist(id)
 );
 
-CREATE TABLE test(
-id VARCHAR(32)
-);
+
 
 insert into userlist values("1",MD5('1'),'1970-1-1 00:00:00');
 insert into settings values("1",0,0,0,0);
@@ -39,8 +40,14 @@ insert into userlist values("4",MD5('4'),'1970-1-1 00:00:00');
 insert into settings values("4",0,0,0,0);
 insert into userlist values("5",MD5('5'),'1970-1-1 00:00:00');
 insert into settings values("5",0,0,0,0);
+
+
+CREATE TABLE test(
+id VARCHAR(32)
+);
 insert into test values("1");
 insert into test values("2");
 insert into test values("3");
 insert into test values("4");
 insert into test values("5");
+
